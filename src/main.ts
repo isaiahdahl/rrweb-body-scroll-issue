8c28e7fbf0d18a918828b311f77ee996b6be805a
isaiah dahl <isaiahdahl@me.com>
2022-08-30 15:10:36 -0700

    min repo issue

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,26 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import * as rrweb from 'rrweb'
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+exportRecording(document.querySelector<HTMLButtonElement>('#export')!)
+
+let events: any[] = [];
+
+rrweb.record({
+  emit(event) {
+    // push event into the events array
+    events.push(event);
+  },
+});
+
+// this function will send events to the backend and reset the events array
+function save() {
+    const replayer = new rrweb.Replayer(events);
+    replayer.play();
+    console.log(JSON.stringify(events));
+}
+
+
+export function exportRecording(element: HTMLButtonElement) {
+    element.addEventListener('click', () => save())
+}
